f8cb1a4434663adc1156820eee27731b16e204b1
Jabke <sereja.gal@yandex.ru>
2024-12-24 17:29:45 +0300

    Commit before merging

---
+++ build/CMakeFiles/success.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/success.dir/main.cpp.o"
+  "CMakeFiles/success.dir/main.cpp.o.d"
+  "success"
+  "success.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/success.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
